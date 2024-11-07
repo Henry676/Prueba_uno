e33febd0a40ffd7d5bb7b2edc086b5f1853a007b
Henry676 <hdhm2001@gmail.com>
2024-11-07 14:03:10 -0600

    Se agrego documentacion al archivo java

--- Hello.java
+++ Hello.java
@@ -3,3 +3,6 @@ public class Hello{
         System.out.println("Hello world");
     }
 }
+//Comandos usados para las ramas de git:
+// Creacion de una rama nueva: git branch <nombre>
+// Cambiar de una rama a otra: git checkout <nombre>
