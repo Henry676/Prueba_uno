ac47c7ec2b604b60b053bdbde73a4d3179f41bdc
Henry676 <hdhm2001@gmail.com>
2024-11-07 14:08:49 -0600

    se agrego la linea 9

--- Hello.java
+++ Hello.java
@@ -6,3 +6,4 @@ public class Hello{
 //Comandos usados para las ramas de git:
 // Creacion de una rama nueva: git branch <nombre>
 // Cambiar de una rama a otra: git checkout <nombre>
+// git checkout -b nombre-de-la-rama: sirve para crear una rama nueva y cambiarse a dicha rama
